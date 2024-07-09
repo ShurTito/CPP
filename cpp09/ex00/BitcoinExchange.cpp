4c1b87292e245e61fd6fafc74d094e237a13cbb6
ShurTito <antoniofdez93@gmail.com>
2024-07-09 16:47:49 +0200

    a

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: antferna <antferna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 17:14:09 by antferna          #+#    #+#             */
-/*   Updated: 2024/07/08 17:15:58 by antferna         ###   ########.fr       */
+/*   Updated: 2024/07/09 12:30:58 by antferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,33 @@
 BitcoinExchange::BitcoinExchange()
 {
    
+}
+
+BitcoinExchange::BitcoinExchange(const std::string &database, const std::string &inputFile)
+{
+}
+
+BitcoinExchange::BitcoinExchange(const BitcoinExchange &other)
+{
+}
+
+BitcoinExchange::~BitcoinExchange()
+{
+}
+
+BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &other)
+{
+    // TODO: insert return statement here
+}
+
+void BitcoinExchange::extractFiles(std::ifstream &fileStream, std::string &dbStream)
+{
+}
+
+void BitcoinExchange::setDbNode(const std::string &dateStr, const double &numDbValue)
+{
+}
+
+void BitcoinExchange::getValueFromDb(const std::string &dateStr, const double &val)
+{
 }
